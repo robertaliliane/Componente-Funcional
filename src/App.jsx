5a31565545d8ea3df284c90a12b3b638973c1769
robertaliliane <robertalilianesoares@outlook.com>
2024-02-28 20:19:45 +0100

    2802

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react" //Importanto o react
+import "./Componente/Componente.css" //Estilizando com Componente.css
+
+function App (){
+  return(
+    <>
+    <h1>Olá, eu sou o Componente Funcional</h1>
+    </>
+  )
+}
+
+export default App
